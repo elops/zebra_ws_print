14f70210817d82892e55e762c3205fb18b688f30
Hrvoje Spoljar <hrvoje.spoljar@gmail.com>
2016-06-03 12:18:37 +0000

    print via curl

--- server.py
+++ server.py
@@ -5,46 +5,20 @@ import logging
 import websockets
 import base64
 import ast
+import json
+
+from aiohttp import web
 
 # commands
 open_cfg = b'{ "open" : "v1.config.zebra.com" }'
 open_raw = b'{ "open" : "v1.raw.zebra.com" }'
+
 print_hello_world = b"""
     ^XA
     ^FT78,76^A0N,28,28^FH\^FDHello\&World^FS
     ^XZ
     """
 cmd1 = b'{}{"weblink.ip.conn1.num_connections":null}'
-
-raw_cmd1 = b"""
-    ^XA
-
-    ^FO140,15
-    ^A0,40,40
-    ^FD
-    Some text!
-    ^FS
-
-    ^FO140,60
-    ^A0,40,40
-    ^FD
-    From some ZPL tut
-    ^FS
-
-    ^FO140,105
-    ^A0,40,40
-    ^FD
-    hrvoje.spoljar@gmail.com
-    ^FS
-
-    ^FO140,150^BY3
-    ^BMN,B,100,Y,N,N
-    ^FD123456^FS
-
-    ^XZ
-    """
-
-
 
 # globals
 printers = {}
@@ -76,14 +50,14 @@ async def consumer(queue, message):
         serial_num = getSerialFromDiscovery(message)
         log.info(' *** MAIN channel established *** ')
         log.info("discovered S/N : {}".format(serial_num))
-        log.info("Addint task to queue : {}".format(open_raw))
         queue.put_nowait(open_raw)
     
     if 'channel_name' in msg_dict.keys():
         if 'v1.raw.zebra.com' == msg_dict['channel_name']:
             serial_num = msg_dict['unique_id']
             log.info(' *** RAW channel established *** ')
-            queue.put_nowait(print_hello_world)
+            printers[serial_num] = queue
+            #queue.put_nowait(print_hello_world)
             #queue.put_nowait(raw_cmd1)
 
         elif 'v1.config.zebra.com' == msg_dict['channel_name']:
@@ -105,6 +79,32 @@ async def producer(queue):
             await asyncio.sleep(2)
 
 
+async def list_printers(request):
+    output = json.dumps(list(printers.keys()))
+    log.info('OUTPUT JSON : {}'.format(output))
+    return web.Response(text=output)
+
+
+async def zprint(request):
+    """ This coro receives print job which is relayed to appropriate queue """
+    post_data = request.content.read_nowait()
+    log.info('POST : {}'.format(post_data))
+    for command in str(post_data).split('&'):
+        printer, print_job_encoded = str(command).split('=')
+        printer = str(printer)
+        print_job = base64.b64decode(print_job_encoded)
+        print("Printer : {}".format(printer))
+        print("Job : {}".format(print_job))
+
+        #print_queue printers[printer]
+        log.info('Printers : {}'.format(printers))
+        print_queue = printers['50J161000398']
+        print_queue.put_nowait(print_job)
+        log.info('Print queue : {}'.format(type(print_queue)))
+
+    return web.Response(text='.')
+
+
 async def handler(websocket, path):
     log.info('New websocket connection')
 
@@ -119,26 +119,30 @@ async def handler(websocket, path):
 
         if listener_task in done:
             message = listener_task.result()
-            #log.info('WS recv <<<< {} : {}'.format(websocket, message))
             await consumer(queue, message)
         else:
             listener_task.cancel()
 
         if producer_task in done:
             message = producer_task.result()
-            #log.info('WS send >>>> {} : {}'.format(websocket, message))
             await websocket.send(message)
         else:
             producer_task.cancel()
 
     
 def main():
-    log.info("Starting websocket server!")
 
+    log.info("Starting aiohttp server")
+    app = web.Application()
+    app.router.add_route('GET', '/list_printers', list_printers)
+    app.router.add_route('POST', '/zprint', zprint)
+
+    log.info("Starting websocket server!")
     loop = asyncio.get_event_loop()
 
     start_server = websockets.serve(handler, 'localhost', 6000, subprotocols=['v1.weblink.zebra.com'], extra_headers={'Content-Length': '0'})
     loop.run_until_complete(start_server)
+    loop.run_until_complete(web.run_app(app))
     loop.run_forever()
     loop.close()
     log.info("Shutting down websocket server")
@@ -153,8 +157,7 @@ if __name__ == '__main__':
     log.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-
     ch.setFormatter(formatter)
-
     log.addHandler(ch)
+
     main()
