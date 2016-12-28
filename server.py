053b263c736397aa4adcb4690c80cc13c81a4569
Hrvoje Spoljar <hrvoje.spoljar@gmail.com>
2016-12-28 14:49:36 +0100

    Improve handling of websocket disconnect

--- server.py
+++ server.py
@@ -219,23 +219,31 @@ async def handler(websocket, path):
     while True:
         listener_task = asyncio.ensure_future(websocket.recv())
         producer_task = asyncio.ensure_future(producer(queue))
-        done, pending = await asyncio.wait(
-            [listener_task, producer_task],
-            return_when=asyncio.FIRST_COMPLETED)
 
-        if listener_task in done:
-            message = listener_task.result()
-            await consumer(queue, id_queue, message)
-        else:
-            listener_task.cancel()
+        try:
+            done, pending = await asyncio.wait(
+                [listener_task, producer_task],
+                return_when=asyncio.FIRST_COMPLETED)
 
-        if producer_task in done:
-            message = producer_task.result()
-            await websocket.send(message)
-            # artificial sleep to slow down printer can be put here if needed
-            #await asyncio.sleep(3)
-        else:
-            producer_task.cancel()
+            if listener_task in done:
+                message = listener_task.result()
+                await consumer(queue, id_queue, message)
+            else:
+                listener_task.cancel()
+
+            if producer_task in done:
+                message = producer_task.result()
+                await websocket.send(message)
+                # artificial sleep to slow down printer can be put here if needed
+                #await asyncio.sleep(3)
+            else:
+                producer_task.cancel()
+
+        except websockets.exceptions.ConnectionClosed as e:
+            log.info('Websocket connection terminated : {}'.format(e))
+            break
+        except Exception as e:
+            log.error('Unexpected exception : {}'.format(e))
 
 
 def get_msgid(message):
